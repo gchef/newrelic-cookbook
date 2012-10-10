8ea4d3a81b713c48b0c6dffe4d7667a47ad3513e
Gerhard Lazu <gerhard@lazu.co.uk>
2012-10-10 08:51:53 +0100

    If WAN is down, DNS resolution won't work

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,5 @@
 install = (node[:newrelic][:action] == "install")
-wan_up = `ping -c 1 -W 1 google.com`.index(/1 (?:packets )?received/)
+wan_up = `ping -c 1 -W 1 8.8.8.8`.index(/1 (?:packets )?received/)
 
 apt_repository "newrelic" do
   uri "http://download.newrelic.com/debian"
